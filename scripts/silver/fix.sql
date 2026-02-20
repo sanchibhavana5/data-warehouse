0235e99daeb368fa66cd24e54bafe5683a5f423e
sanchibhavana5 <sanchibhavana5@gmail.com>
2026-02-20 14:11:02 +0530

    Update fix.sql

--- scripts/silver/fix.sql
+++ scripts/silver/fix.sql
@@ -1,4 +1,6 @@
 
+----inserting the data into silver.crm_cust_info ---
+
 insert into silver.crm_cust_info(
 cst_id,
 cst_key,
@@ -32,3 +34,38 @@ order by cst_create_date desc
 from bronze.crm_cust_info
 ) as t
 where cust_rank_by_date = 1 and cst_id is not null;
+
+
+
+
+----inserting the data into silver.crm_prd_info ---
+
+
+insert into silver.crm_prd_info(
+    prd_id,
+    cat_id,
+    prd_key,
+    prd_nm,
+    prd_cost,
+    prd_line,
+    prd_start_dt,
+    prd_end_dt 
+)
+
+select 
+prd_id ,
+replace( substring(prd_key,1,5),'-','_') as cat_id,
+substring(prd_key,7,len(prd_key)) as prd_key,
+prd_nm,
+isnull (prd_cost,0) as prd_cost,
+case
+ WHEN UPPER(trim(prd_line)) ='M' THEN 'Mountain' 
+ WHEN UPPER(trim(prd_line)) ='S' THEN 'Other Sales' 
+  WHEN UPPER(trim(prd_line)) ='T' THEN 'Touring'
+   WHEN UPPER(trim(prd_line)) ='R' THEN 'Road' 
+else 'n/a'
+end as prd_line,
+cast (prd_start_dt as date) as prd_start_dt,
+cast( lead(prd_start_dt) over(partition by prd_key order by prd_start_dt)-1 as date ) as prd_end_dt
+from
+bronze.crm_prd_info;
